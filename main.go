1f074f824a0daa69c8c0364b8473973e80328db1
Dylan Meeks <dylan.meeks@aol.com>
2026-05-15 18:45:20 -0700

    chatgpt website start

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	// "errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	// "os"
-	// "regexp"
 )
 
 func main() {
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.Handle(
+		"/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))),
+	)
 
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/projects", projectsHandler)
@@ -21,34 +21,56 @@ func main() {
 	http.HandleFunc("/about", aboutHandler)
 
 	fmt.Println("Server running on http://localhost:8080")
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("templates/" + tmpl)
+func renderTemplate(w http.ResponseWriter, tmpl string, data any) {
+	t, err := template.ParseFiles(
+		"templates/base.html",
+		"templates/"+tmpl,
+	)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	t.Execute(w, nil)
+
+	err = t.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.html")
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	renderTemplate(w, "home.html", map[string]string{
+		"Title": "Home",
+	})
 }
 
 func projectsHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "projects.html")
+	renderTemplate(w, "projects.html", map[string]string{
+		"Title": "Projects",
+	})
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "contact.html")
+	renderTemplate(w, "contact.html", map[string]string{
+		"Title": "Contact",
+	})
 }
 
 func skillsHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "skills.html")
+	renderTemplate(w, "skills.html", map[string]string{
+		"Title": "Skills",
+	})
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.html")
+	renderTemplate(w, "about.html", map[string]string{
+		"Title": "About",
+	})
 }
